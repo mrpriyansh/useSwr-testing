45241ca4bcb98f44d604ae054ff18520d4212d2c
Gaharana <mr.priyanshgaharana@gmail.com>
2020-05-02 10:50:00 +0530

    swr

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useSWR from 'swr';
 
 function App() {
+  
+  // useEffect(()=>{
+  //   fetch('http://34.66.137.51:4000/testing',{
+  //     method:'GET',
+  //     header:{ "Content-Type" : "applicaton/json"}
+  //   })
+  //   .then(response=>response.json())
+  //   .then(data=>{
+  //     console.log(data);
+  //   })
+  // },[])
+  const {data, error} = useSWR('http://34.66.137.51:4000/testing',fetch('http://34.66.137.51:4000/testing',{
+    method:'GET',
+    header:{ "Content-Type" : "applicaton/json"}
+    })
+    .then(response=>response.json())
+  );
+  useEffect(()=>{
+    console.log(data);
+  },[data]);
+  if(error) return <div> Failed to load </div>
+  if(!data) return <div> Loading ...</div> 
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +33,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{data}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
